f53cd0c443ceb53a77413941acf4f0f6180ddb44
samirametin <115466868+samirametin@users.noreply.github.com>
2023-10-19 23:24:23 +0400

    adding while loop for playing lots of attemps

--- main.py
+++ main.py
@@ -8,14 +8,28 @@ if difficulty == 'easy':
     number_of_attemps = 10
 elif difficulty == 'hard':
     number_of_attemps = 5
-print(f"You have {number_of_attemps} attemps remainig to guess the number. ")
 
-guess = int(input("Make a guess: "))
 
-if number < guess:
-    print(f"Too high. ")
-    
-elif number > guess:
-    print("Too low. ")
-else:
-    print("You guessed the number. ")
+is_game_over = False
+
+while not is_game_over:
+    print(f"You have {number_of_attemps} attemps remainig to guess the number. ")
+    guess = int(input("Make a guess: "))
+
+    if number < guess:
+        print("Too high. \nGuess again")
+        number_of_attemps -= 1
+    elif number > guess:
+        print("Too low. \nGuess again")
+        number_of_attemps -= 1
+    else:
+        print("You guessed the number.")
+        is_game_over = True
+
+    if number_of_attemps == 0:
+        print("You've run out of guesses, you lose. ")
+        is_game_over = True
+
+
+
+
