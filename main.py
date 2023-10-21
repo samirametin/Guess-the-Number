0e8af3092f2c2e6e60755e2c451bd637dbc5fc7d
samirametin <115466868+samirametin@users.noreply.github.com>
2023-10-21 23:16:05 +0400

    turning game into functions

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
 from random import randint
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
-number = randint(1,100)
 
-if difficulty == 'easy':
-    number_of_attemps = 10
-elif difficulty == 'hard':
-    number_of_attemps = 5
+EASY_GAME = 10
+HARD_GAME = 5
 
 
-is_game_over = False
+def set_difficulty():
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if difficulty == "easy":
+        return EASY_GAME
+    elif difficulty == "hard":
+        return HARD_GAME
 
-while not is_game_over:
-    print(f"You have {number_of_attemps} attemps remainig to guess the number. ")
-    guess = int(input("Make a guess: "))
 
-    if number < guess:
+def check_answer(answer, guess, turns):
+    if answer < guess:
         print("Too high.")
-        number_of_attemps -= 1
-    elif number > guess:
+        return turns - 1
+    elif answer > guess:
         print("Too low.")
-        number_of_attemps -= 1
+        return turns - 1
     else:
-        print(f"You guessed the number. The number was {number}")
-        is_game_over = True
-
-    if number_of_attemps == 0:
-        print("You've run out of guesses, you lose. ")
-        is_game_over = True
-    else:
-        print("Guess again. ")
+        print(f"You guessed the number. The number was {answer}")
+        return
 
 
+def game():
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    number = randint(1, 100)
+    print(f"PSsst the number is {number}")
+    attemps = set_difficulty()
+    guess = 0
+    while number != guess:
+        print(f"You have {attemps} attemps remainig to guess the number. ")
+        guess = int(input("Make a guess: "))
+        attemps = check_answer(number, guess, attemps)
+        if attemps == 0:
+            print("You've run out of guesses, you lose. ")
+            return
+        elif number != guess:
+            print("Guess again. ")
 
 
+game()
